994222ad7629897ce62451546ea0959b58e78500
GuohaoCui <100395885+GuohaoCui@users.noreply.github.com>
2024-10-20 19:29:34 +0800

    Update eval_diffusion.py

--- An_Enhanced_Diffusion-based_Network_for_Efficient_Stamp_Removal_main/eval_diffusion.py
+++ An_Enhanced_Diffusion-based_Network_for_Efficient_Stamp_Removal_main/eval_diffusion.py
@@ -11,7 +11,7 @@ from models import DenoisingDiffusion, DiffusiveRestoration
 def config_get():
     parser = argparse.ArgumentParser()
     # 参数配置文件路径
-    parser.add_argument("--config", default='configs.yml', type=str, required=False, help="Path to the config file")
+    parser.add_argument("--config", default='/public/home/lab70432/CGH_workspace/diffusion_stamp_main_STN/configs.yml', type=str, required=False, help="Path to the config file")
     args = parser.parse_args()
 
     with open(os.path.join(args.config), "r") as f:
@@ -59,6 +59,40 @@ def main():
     # 恢复图像
     model.restore(val_loader, r=config.data.grid_r)
 
+def resize_images_from_file(folder, input_file):
+    """
+    Resize images in the specified folder according to the sizes recorded in the input text file.
+
+    :param folder: The folder containing the images to resize.
+    :param input_file: The text file with recorded image names and their original sizes.
+    """
+    
+    # 读取输入文件中所有行的信息，并存储在字典中
+    size_dict = {}
+    with open(input_file, 'r') as file:
+        for line in file:
+            # 移除换行符，然后分割文件名和尺寸
+            parts = line.strip().split(': ')
+            filename = parts[0]
+            # 假设尺寸格式正确，并分割宽度和高度
+            size = parts[1].split('x')
+            width, height = int(size[0]), int(size[1])
+            size_dict[filename] = (width, height)
+    
+    # 遍历文件夹中的图像，如果其尺寸信息在字典中，则调整尺寸
+    for filename in os.listdir(folder):
+        if filename in size_dict:
+            filepath = os.path.join(folder, filename)
+            new_size = size_dict[filename]
+            with Image.open(filepath) as img:
+                resized_img = img.resize(new_size, Image.ANTIALIAS)
+                resized_img.save(filepath)
+
+    print(f"Images have been resized according to {input_file}")
+
 
 if __name__ == '__main__':
     main()
+    path = '/public/home/lab70432/CGH_workspace/diffusion_stamp_main_STN/results'
+    input_file = '/public/home/lab70432/CGH_workspace/output.txt'
+    resize_images_from_file(path, input_file)    
